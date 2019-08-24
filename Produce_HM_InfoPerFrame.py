65b324f7af2688a6aa43a4f7dafe38eb2aeef782
mkjubran <mjubran@birzeit.edu>
2019-08-24 11:46:53 +0000

    Ver0.6

--- Produce_HM_InfoPerFrame.py
+++ Produce_HM_InfoPerFrame.py
@@ -1,5 +1,3 @@
-#python ./Compare_Rate_PSNR_Frame.py --fn1=../HMSsync/HMSResu --fn2=../HMSsync/HMSRe
-
 from __future__ import division
 import numpy as np
 import os, sys, subprocess, pdb
@@ -18,12 +16,12 @@ parser.add_argument('--f',nargs='?',default ='park_joy_640480.mp4', type=str, he
 parser.add_argument('--qp',nargs='?',default =30, type=int, help='qp value [30]')
 parser.add_argument('--mcu',nargs='?',default =64, type=int, help='maximum CU size [64]')
 parser.add_argument('--mpd',nargs='?',default =4, type=int, help='maximum partition depth [4]')
-parser.add_argument('--nf',nargs='?',default =3000000, type=int, help='number of frames to be encoded [3000000]')
+parser.add_argument('--nf',nargs='?',default =3000000, type=int, help='number of frames to be encoded [900000000]')
 parser.add_argument('--fps',nargs='?',default =30, type=int, help='frames per second [30]')
 parser.add_argument('--path',nargs='?',default ='../HMLC_Results/', type=str, help='path for results [../HMLC_Results]')
 parser.add_argument('--w',nargs='?',default =640, type=int, help='width [640]')
 parser.add_argument('--h',nargs='?',default =480, type=int, help='hight [480]')
-parser.add_argument('--rate',nargs='?',default =10000, type=int, help='rate [10000]')
+parser.add_argument('--rate',nargs='?',default =10000, type=str, help='rate [100k]')
 parser.add_argument('--cfg',nargs='?',default ='./encoder_lowdelay_P_main.cfg', type=str, help='configuration file [./encoder_lowdelay_P_main.cfg]')
 args = parser.parse_args()
 
@@ -73,10 +71,10 @@ def produce_training_data(finfo):
     fb = open(video_path+'/HMLCPOC/rate.bin','wb')
     pickle.dump(Bits,fb)
     fb.close()
-    print(Bits)
-    fb = open(video_path+'/HMLCPOC/rate.bin','rb')
-    Bitsnew=pickle.load(fb)
-    print(Bitsnew)
+#    print(Bits)
+#    fb = open(video_path+'/HMLCPOC/rate.bin','rb')
+#    Bitsnew=pickle.load(fb)
+#    print(Bitsnew)
     return
 
 ###--------------------------------------------------------------
@@ -94,26 +92,38 @@ if __name__ == '__main__':
  rate=args.rate;
  cfg=args.cfg;
 
+ if (rate[-1]=='m') or (rate[-1]=='M'):
+   rate=int(rate[:-1])*1000000
+ elif (rate[-1]=='k') or (rate[-1]=='K'):
+   rate=int(rate[:-1])*1000
+ else:
+  rate=int(rate)
+ #pdb.set_trace();
+
  vid=filename.split('/')[-1]
  video_path=path+vid[:-4]+'/'
-# osout = call('rm -rf {}'.format(path))
-# osout = call('mkdir {}'.format(path[:-1]))
-# osout = call('mkdir {}'.format(video_path[:-1]))
-# osout = call('rm -rf {}'.format(video_path+'/POC'))
-# osout = call('mkdir {}'.format(video_path+'/POC'))
-# osout = call('rm -rf {}'.format(video_path+'/HMLCPOC'))
-# osout = call('mkdir {}'.format(video_path+'/HMLCPOC'))
+ osout = call('rm -rf {}'.format(path))
+ osout = call('mkdir {}'.format(path[:-1]))
+ osout = call('mkdir {}'.format(video_path[:-1]))
+ osout = call('rm -rf {}'.format(video_path+'/POC'))
+ osout = call('mkdir {}'.format(video_path+'/POC'))
+ osout = call('rm -rf {}'.format(video_path+'/HMLCPOC'))
+ osout = call('mkdir {}'.format(video_path+'/HMLCPOC'))
 
-# export_frames(filename);
-# osout = call('ffmpeg -y -i {} -vcodec rawvideo -pix_fmt yuv420p {}'.format(filename,filename[:-3]+'yuv'))
+ export_frames(filename);
+ osout = call('ffmpeg -y -i {} -vcodec rawvideo -pix_fmt yuv420p {}'.format(filename,filename[:-3]+'yuv'))
 
- if (rate == 0 ):
+ if ( rate == 0 ):
    ratectl=0
  else:
    ratectl=1
 
-# osout = call('rm -rf ../vid/HMEncodedVideo.bin')
-# osout = call('rm -rf encoder.log')
-# osout = call('./HM/bin/TAppEncoderStatic -c {} --InputFile={} --SourceWidth={} --SourceHeight={} --SAO=1 --QP={} --FrameRate={} --FramesToBeEncoded={} --MaxCUSize={} --MaxPartitionDepth={} --QuadtreeTULog2MaxSize=4 --BitstreamFile={} --RateControl={} --TargetBitrate={}'.format(cfg,filename[:-3]+'yuv',w,h,qp,fps,nf,mcu,mpd,path+'/HMEncoded.bin',ratectl,rate))
-# osout = call('mv HMLC_InfoPerFrame.txt {}'.format(video_path+'/HMLCPOC/HMLC_InfoPerFrame.txt'))
+ if ( nf == 0 ):
+   nf=900000000
+
+
+ osout = call('rm -rf ../vid/HMEncodedVideo.bin')
+ osout = call('rm -rf encoder.log')
+ osout = call('./HM/bin/TAppEncoderStatic -c {} --InputFile={} --SourceWidth={} --SourceHeight={} --SAO=1 --QP={} --FrameRate={} --FramesToBeEncoded={} --MaxCUSize={} --MaxPartitionDepth={} --QuadtreeTULog2MaxSize=4 --BitstreamFile={} --RateControl={} --TargetBitrate={}'.format(cfg,filename[:-3]+'yuv',w,h,qp,fps,nf,mcu,mpd,path+'/HMEncoded.bin',ratectl,rate))
+ osout = call('mv HMLC_InfoPerFrame.txt {}'.format(video_path+'/HMLCPOC/HMLC_InfoPerFrame.txt'))
  produce_training_data(video_path+'/HMLCPOC/HMLC_InfoPerFrame.txt')
